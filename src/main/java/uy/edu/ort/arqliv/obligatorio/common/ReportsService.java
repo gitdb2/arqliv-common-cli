e0af79191f59df7c9766c7e54c99ee9a511c2bc4
mauricio <karbunkle@gmail.com>
2014-06-23 23:08:49 -0300

    reportes departure

--- src/main/java/uy/edu/ort/arqliv/obligatorio/common/ReportsService.java
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/common/ReportsService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import uy.edu.ort.arqliv.obligatorio.common.exceptions.CustomServiceException;
 import uy.edu.ort.arqliv.obligatorio.dominio.Arrival;
+import uy.edu.ort.arqliv.obligatorio.dominio.Departure;
 
 public interface ReportsService {
 
@@ -25,5 +26,24 @@ public interface ReportsService {
 	 * @throws CustomServiceException
 	 */
 	public List<Arrival> arrivalsByMonthByShip(String user, int month, Long shipId) throws CustomServiceException;
+	
+	/**
+	 *  Retorna los departures que para un mes (1 Enero)
+	 * @param user
+	 * @param month
+	 * @return
+	 * @throws CustomServiceException
+	 */
+	public List<Departure> departuresByMonth(String user, int month) throws CustomServiceException;
+	
+	/**
+	 * Lista los departures filtrando por me y por id barco
+	 * @param user
+	 * @param month
+	 * @param shipId
+	 * @return
+	 * @throws CustomServiceException
+	 */
+	public List<Departure> departuresByMonthByShip(String user, int month, Long shipId) throws CustomServiceException;
 	
 }
